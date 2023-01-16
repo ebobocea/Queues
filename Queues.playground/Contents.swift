42b41f4c4299a479739584b478eab600b583f1f2
Elisei Bobocea <elisey93@icloud.com>
2023-01-16 14:32:39 +0000

    first implementation

--- Queues.playground/Contents.swift
+++ Queues.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Queue<T> {
+    private var array: [T] = []
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    var count: Int {
+        return array.count
+    }
+    
+    mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        return array.isEmpty ? nil : array.removeFirst()
+    }
+    
+    func peek() -> T? {
+        return array.first
+    }
+}
+
